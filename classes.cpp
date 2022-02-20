33c940e6f2c96250e0b62ba068d7484305e23d21
Nenad Korolija <nenadko@gmail.com>
2022-02-20 19:14:15 +0100

    Removed add_neighbor_couple, kept add_neighbor

--- classes.cpp
+++ classes.cpp
@@ -40,7 +40,7 @@ bool KSimplex::find_neighbor(KSimplex *k1){
     return false;
 }
 
-void KSimplex::add_neighbor_couple(KSimplex *k1){
+void KSimplex::add_neighbor(KSimplex *k1){
     if(!k1)
         return;
     int kK1 = k1->k;
@@ -48,13 +48,6 @@ void KSimplex::add_neighbor_couple(KSimplex *k1){
         neighbors->elements[kK1].push_back(k1);
     if(!k1->find_neighbor(this))
         k1->neighbors->elements[k].push_back(this);
-}
-
-void KSimplex::add_neighbor(KSimplex *k1){
-    if(!k1)
-        return;
-    int kK1 = k1->k;
-    add_neighbor_couple(k1);
     if(kK1){
         for(auto &it : k1->neighbors->elements[kK1 - 1])
             add_neighbor(it);
