6e4901917c24992b26baabbbd4f7078641911693
Dusan Cvijetic <dusancvijetic2000@gmail.com>
2022-03-07 02:12:45 +0100

    Corrected name convention inconsistencies.

--- input-and-output.cpp
+++ input-and-output.cpp
@@ -18,69 +18,69 @@ void save_complex_to_xml_file(SimpComp* simpComp, const string& filename)
 
     using namespace rapidxml;
 
-    int current_maxid = UniqueIDColor::next_free_uid_number;
+    int currentMaxid = UniqueIDColor::next_free_uid_number;
     UniqueIDColor::colorize_entire_complex(simpComp);
 
-    xml_document<> tree_xml;
-    xml_node<>* base = tree_xml.allocate_node(node_element, simpComp->name.c_str());
-    tree_xml.append_node(base);
+    xml_document<> treeXml;
+    xml_node<>* base = treeXml.allocate_node(node_element, simpComp->name.c_str());
+    treeXml.append_node(base);
 
     char* nameCString = "name";
-    xml_node<>* nameNode = tree_xml.allocate_node(node_element, nameCString, simpComp->name.c_str());
+    xml_node<>* nameNode = treeXml.allocate_node(node_element, nameCString, simpComp->name.c_str());
     base->append_node(nameNode);
 
     char* dimensionCString = "dimension";
-    const char* DCString = tree_xml.allocate_string(to_string(simpComp->D).c_str(), to_string(simpComp->D).length() + 1);
-    xml_node<>* dimensionNode = tree_xml.allocate_node(node_element, dimensionCString, DCString);
+    const char* DCString = treeXml.allocate_string(to_string(simpComp->D).c_str(), to_string(simpComp->D).length() + 1);
+    xml_node<>* dimensionNode = treeXml.allocate_node(node_element, dimensionCString, DCString);
     base->append_node(dimensionNode);
 
-    vector<xml_node<>*> levels = get_element_levels_as_xml_nodes(simpComp, tree_xml);
+    vector<xml_node<>*> levels = get_element_levels_as_xml_nodes(simpComp, treeXml);
     for (auto nd : levels) base->append_node(nd);
 
-    string nd_str;
-    string color_type_str;
-    string color_val_str;
+    string ndStr;
+    string colorTypeStr;
+    string colorValStr;
     char* levelCStr = "level";
     for (unsigned int lvl = 0; lvl < simpComp->elements.size(); lvl++) {
         for (auto ks : simpComp->elements[lvl]) {
-            nd_str = "ksimplex " + ks->colors.back()->get_color_value_as_str();
-            char* nd_cstr = tree_xml.allocate_string(nd_str.c_str(), nd_str.length() + 1);
-            xml_node<>* ks_nd = tree_xml.allocate_node(node_element, nd_cstr);
-            base->append_node(ks_nd);
+            ndStr = "ksimplex " + ks->colors.back()->get_color_value_as_str();
+            char* ndCstr = treeXml.allocate_string(ndStr.c_str(), ndStr.length() + 1);
+            xml_node<>* ksNd = treeXml.allocate_node(node_element, ndCstr);
+            base->append_node(ksNd);
 
-            char* lvlCStr = tree_xml.allocate_string(to_string(lvl).c_str(), to_string(lvl).length() + 1);
-            xml_node<>* lvlNd = tree_xml.allocate_node(node_element, levelCStr, lvlCStr);
-            ks_nd->append_node(lvlNd);
+            char* lvlCStr = treeXml.allocate_string(to_string(lvl).c_str(), to_string(lvl).length() + 1);
+            xml_node<>* lvlNd = treeXml.allocate_node(node_element, levelCStr, lvlCStr);
+            ksNd->append_node(lvlNd);
 
             for (auto it = ks->colors.begin(); it + 1 != ks->colors.end(); it++) {
-                color_type_str = to_string((*it)->type);
-                char* color_type_cstr = tree_xml.allocate_string(color_type_str.c_str(), color_type_str.length() + 1);
-                color_val_str = (*it)->get_color_value_as_str();
-                char* color_val_cstr = tree_xml.allocate_string(color_val_str.c_str(), color_val_str.length() + 1);
-                xml_node<>* color_nd = tree_xml.allocate_node(node_element, color_type_cstr, color_val_cstr);
+                colorTypeStr = to_string((*it)->type);
+                char* colorTypeCstr = treeXml.allocate_string(colorTypeStr.c_str(), colorTypeStr.length() + 1);
+                colorValStr = (*it)->get_color_value_as_str();
+                char* colorValCstr = treeXml.allocate_string(colorValStr.c_str(), colorValStr.length() + 1);
+                xml_node<>* color_nd = treeXml.allocate_node(node_element, colorTypeCstr, colorValCstr);
 
-                ks_nd->append_node(color_nd);
+                ksNd->append_node(color_nd);
             }
 
-            vector<xml_node<>*> neighbors = get_element_levels_as_xml_nodes(ks->neighbors, tree_xml);
-            for (auto nd : neighbors) ks_nd->append_node(nd);
+            vector<xml_node<>*> neighbors = get_element_levels_as_xml_nodes(ks->neighbors, treeXml);
+            for (auto nd : neighbors) ksNd->append_node(nd);
         }
     }
 
     // We are done with temporary UniqueIDColors, so we delete them:
     for (auto lvl : simpComp->elements) {
         for (auto ks : lvl) {
-            Color* to_be_deleted = ks->colors.back();
+            Color* toBeDeleted = ks->colors.back();
             ks->colors.pop_back();
-            delete to_be_deleted;
+            delete toBeDeleted;
         }
     }
 
 
-    ofstream output_file;
-    output_file.open(filename);
-    output_file << tree_xml;
-    output_file.close();
+    ofstream outputFile;
+    outputFile.open(filename);
+    outputFile << treeXml;
+    outputFile.close();
 }
 
 
@@ -88,31 +88,31 @@ vector<rapidxml::xml_node<>*> get_element_levels_as_xml_nodes(SimpComp* simpComp
 {
     using namespace rapidxml;
 
-    vector<xml_node<>*> nodes_xml;
+    vector<xml_node<>*> nodesXml;
 
-    string lvl_str;
-    string simps_str;
+    string lvlStr;
+    string simpsStr;
     bool first;
 
     for (unsigned int lvl = 0; lvl < simpComp->elements.size(); lvl++) {
         // TODO: Memory leaking problems.
 
-        lvl_str = "level " + to_string(lvl);
-        char* lvl_cstr = mp.allocate_string(lvl_str.c_str(), lvl_str.length() + 1);
+        lvlStr = "level " + to_string(lvl);
+        char* lvlCstr = mp.allocate_string(lvlStr.c_str(), lvlStr.length() + 1);
 
-        simps_str = "";
+        simpsStr = "";
         first = true;
         for (auto ksimp : simpComp->elements[lvl]) {
             if (!first) {
-                simps_str += ",";
+                simpsStr += ",";
             }
             else first = false;
-            simps_str += ksimp->colors.back()->get_color_value_as_str();      // Assuming no sorting is done.
+            simpsStr += ksimp->colors.back()->get_color_value_as_str();      // Assuming no sorting is done.
         }
-        char* simps_cstr = mp.allocate_string(simps_str.c_str(), simps_str.length() + 1);
+        char* simpsCstr = mp.allocate_string(simpsStr.c_str(), simpsStr.length() + 1);
 
-        nodes_xml.push_back(mp.allocate_node(node_element, lvl_cstr, simps_cstr));
+        nodesXml.push_back(mp.allocate_node(node_element, lvlCstr, simpsCstr));
     }
 
-    return nodes_xml;
+    return nodesXml;
 }
