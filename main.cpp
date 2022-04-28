c0a54debc8cccb6498e3376d403f8739023a01de
Nenad Korolija <nenadko@gmail.com>
2022-04-29 00:11:03 +0200

    Cleaning

--- main.cpp
+++ main.cpp
@@ -35,35 +35,46 @@ int main(){
 //*  
     cout << endl << "---------------------------------------------------"
             << endl << "General seed example, compact printing:" << endl << endl;
-    // seed_single_simplex seeds a simplex or a sphere (last argument == 1):
-    SimpComp *simpComp = seed_single_simplex("1. general Simplex", 3, 0);
+
+    SimpComp *simpComp = seed_single_simplex(3);
     simpComp->print_compact();
 
+    //SimpComp *simpComp = seed_sphere(3);
+    //simpComp->print_compact();
+
+	/*
+	//Function reconstruct_neighbors_from_vertices demonstration:
+	
+    SimpComp *simpComp = seed_single_simplex(3);
+    simpComp->print_compact();
+
+    // Print random k-simplex:
 	cout << endl;
     simpComp->elements[1][0]->print("");
 	cout << endl;
 
+    // Delete neighbors except vertices:
 	for(int k = 1; k <= simpComp->D; k++)
 		for(auto &kSimplex : simpComp->elements[k])
 			for(int tempK = 1; tempK <= simpComp->D; tempK++)
 				kSimplex->neighbors->elements[tempK].clear();
 
+    // Print the same k-simplex:
 	cout << endl;
     simpComp->elements[1][0]->print("");
 	cout << endl;
 
-
 	KSimplex::reconstruct_neighbors_from_vertices(simpComp);
 
+    // Print the same k-simplex:
 	cout << endl;
     simpComp->elements[1][0]->print("");
 	cout << endl;
 
 	cout << endl;
     simpComp->print_compact();
-
-
-//*/
+    //*/
+    
     return 0;
 }
 
