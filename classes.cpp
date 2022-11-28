833e512cba81dee03d218dc53435334be8e83cbb
dyco <dusancvijetic2000@gmail.com>
2022-11-29 00:48:50 +0100

    Deleting old files.

--- classes.cpp
+++
@@ -1,6 +0,0 @@
-
-// ######################
-// This is an obsolete file, that does nothing. Ignore it.
-// Implementations of class functions have been moved to other files.
-// ######################
-
--- gui/DrawComplexGLWidget.cpp
+++
@@ -1,1 +0,0 @@
-#include "DrawComplexGLWidget.h"
