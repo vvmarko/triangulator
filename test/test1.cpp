b5f219ced4d124731c15ca01ed8cb922b617e0c8
dyco <dusancvijetic2000@gmail.com>
2022-11-30 19:52:08 +0100

    Testing added.

--- test/test1.cpp
+++ test/test1.cpp
@@ -10,9 +10,10 @@ int main(){
 
     SimpComp *simpComp = seed_sphere_intuitively(3, "moja_sfera");
     UniqueIDColor::colorize_entire_complex(simpComp);
-    save_complex_to_xml_file(simpComp, "test1.xml");
-    SimpComp *simpComp2 = read_complex_from_xml_file("test1.xml");
-    save_complex_to_xml_file(simpComp2, "test2.xml");
+    //save_complex_to_xml_file(simpComp, "test1.xml");
+    //SimpComp *simpComp2 = read_complex_from_xml_file("test1.xml");
+    //save_complex_to_xml_file(simpComp2, "test2.xml");
+    simpComp->print_detailed();
 
 
   cout << endl << "#################################################" << endl;
