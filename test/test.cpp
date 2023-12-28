f90b8b08a00b8b657352308bf7e01b9d9ddc7af8
Nenad Korolija <nenadko@gmail.com>
2023-12-28 11:51:41 +0100

    Added tests and dummy tests for KSimplex

--- test/test.cpp
+++ test/test.cpp
@@ -635,15 +635,15 @@ Simplices at level k = 2:
 
 
 void test_unseed_complex(){
-    string name = "test";
-    int D = 2;
+    //string name = "test";
+    //int D = 2;
 
     // Initilize simplicial complex of dimension D, and an empty k-simplex:
-    SimpComp *simpComp = new(nothrow) SimpComp(name,D);
+    //SimpComp *simpComp = new(nothrow) SimpComp(name,D);
 
-    KSimplex *simpsmall = simpComp->create_ksimplex(0);
+    //KSimplex *simpsmall = simpComp->create_ksimplex(0);
     
-    KSimplex *newKSimplex = build_simplex_one_level_up(simpComp, simpsmall);    
+    //KSimplex *newKSimplex = build_simplex_one_level_up(simpComp, simpsmall);    
 
     //newKSimplex->print();
 
@@ -662,6 +662,373 @@ void test_unseed_everything(){
 }
 
 
+
+
+
+// Collect pointers to my neighboring vertices (zero-level neighbors) into a set
+// (if I am a vertex myself, but put me into the set --- this is convenient
+// for vertices, despite that no simplex is a neighbor of itself)
+void test_KSimplex_collect_vertices(){
+    set<KSimplex*> s;
+
+    int D = 2;
+
+    SimpComp *sphere = seed_sphere_intuitively(D, "Test sphere");
+    if(sphere==nullptr){
+        log_report(LOG_ERROR,"Unable to seed a sphere, nullptr generated, aborting test!");
+    }
+
+    UniqueIDColor::colorize_simplices_at_level(sphere, 0);
+     
+    //sphere->print();
+    
+    // Otherwise, collect neighboring vertices into a set:
+    sphere->elements[2][0]->collect_vertices(s);
+
+    TEST_FAILED_IF(3 != s.size(), "test_seed_sphere_intuitively");
+
+    string expectedStr = R""""(Printing KSimplex:  k = 0, D = 2
+Printing colors:
+  Printing neighbors:
+Simplices at level k = 0:   
+Simplices at level k = 1:   (7-8), (7-9), (7-10)
+Simplices at level k = 2:   (7-8-9), (7-8-10), (7-9-10)
+Printing KSimplex:  k = 0, D = 2
+Printing colors:
+  Printing neighbors:
+Simplices at level k = 0:   
+Simplices at level k = 1:   (7-8), (8-9), (8-10)
+Simplices at level k = 2:   (7-8-9), (7-8-10), (8-9-10)
+Printing KSimplex:  k = 0, D = 2
+Printing colors:
+  Printing neighbors:
+Simplices at level k = 0:   
+Simplices at level k = 1:   (7-9), (8-9), (9-10)
+Simplices at level k = 2:   (7-8-9), (7-9-10), (8-9-10)
+)"""";
+
+    // Redirect cout:
+    streambuf* oldCoutStreamBuf = cout.rdbuf();
+    std::ostringstream testStr;
+    cout.rdbuf( testStr.rdbuf() );
+
+    // All cout output is redirected to testStr:
+    for(auto &it : s)
+        it->print();
+
+    // Restoring original cout:
+    cout.rdbuf( oldCoutStreamBuf );
+ 
+    // Printing all redirected cout output:
+    //cout << testStr.str();
+    //cout << expectedStr;
+
+    TEST_FAILED_IF(expectedStr != testStr.str(), "test_KSimplex_collect_vertices");
+
+    test_ok("test_KSimplex_collect_vertices");
+}
+
+
+
+// Verify if a given simplex is my neighbor
+// (true if it is found in my neighbor list, false otherwise)
+void test_KSimplex_find_neighbor(){
+    set<KSimplex*> s;
+
+    int D = 2;
+
+    SimpComp *sphere = seed_sphere_intuitively(D, "Test sphere");
+    if(sphere==nullptr){
+        log_report(LOG_ERROR,"Unable to seed a sphere, nullptr generated, aborting test!");
+    }
+
+    UniqueIDColor::colorize_simplices_at_level(sphere, 0);
+     
+    TEST_FAILED_IF(sphere->elements[1][0]->find_neighbor(sphere->elements[1][1]), "test_KSimplex_find_neighbor");
+
+    test_ok("test_KSimplex_find_neighbor");
+}
+
+
+// #####################################
+// Functions for manipulating neighbors:
+// #####################################
+
+// Establish a neighborhood relationship between a given simplex and myself
+// NOTE: This is a powerful function, that does a lot of things:
+//  - it adds the simplex into my list of neighbors (if it is not there already)
+//  - it adds myself to the list of neighbors of that simplex (if I am not there already)
+//  - if my level is smaller than the level of that simplex, add all my subsimplices as its neighbors
+//  - if its level is smaller than mine, add all of its subsimplices as my neighbors
+void test_KSimplex_add_neighbor(){
+    // tested with:
+    //test_kSimplex_find_neighbor();
+  
+}
+
+// Disconnect a given kSimplex from my list of neighbors, and disconnect me from his list
+// NOTE: This function is *not* a full inverse of add_neighbor(), since it does not
+// disconnect the sub-neighbors of the two simplices (and it should not!)
+void test_KSimplex_delete_neighbor(){
+}
+
+// Disconnect all simplices from my list of neighbors, and disconnect me from everyone else's lists
+// NOTE: This function calls delete_neighbor() for all simplices in my list of neighbors, so
+// the same caveats apply here as for the above function
+void test_KSimplex_delete_all_neighbors(){
+}
+
+// Reconstruct my neighbors table if my neighbor vertices are known
+// NOTE: This function traverses the entire given complex, and adds as my neighbors all
+// other simplices which have compatible vertex structure. Namely, it finds my
+// sub-neighbor if all vertices of a given simplex are a subset of my simplices, and it
+// finds my super-neighbor if all my vertices are a subset of a given simplex.
+// NOTE: It is important to emphasize that this function implicitly assumes that
+// each simplex in the complex has its vertices already assigned in its appropriate
+// list of neighbors, otherwise it will fail to find some (or all) neighbors, and in
+// addition it may also find false-positives, and assign neighbors that should not be
+// neighbors. The assumption about vertices is semantic (it depends on the type of the
+// complex in question) and thus it is impossible to verify. Therefore, you have to
+// make sure yourself that this assumption is satisfied for your complex, before
+// calling this function.
+void test_KSimplex_reconstruct_neighbors_from_vertices(){
+    set<KSimplex*> s;
+
+    int D = 2;
+
+    SimpComp *sphere = seed_sphere_intuitively(D, "Test sphere");
+    if(sphere==nullptr){
+        log_report(LOG_ERROR,"Unable to seed a sphere, nullptr generated, aborting test!");
+    }
+
+    UniqueIDColor::colorize_simplices_at_level(sphere, 0);
+
+    //cout << "aaaaaaaaaaaaaaa" << endl;
+    //sphere->print();
+    //cout << "bbbbbbbbbbbbbbb" << endl;
+     
+    sphere->elements[0][0]->delete_all_neighbors();
+
+    //cout << "aaaaaaaaaaaaaaa" << endl;
+    //sphere->print();
+    //cout << "bbbbbbbbbbbbbbb" << endl;
+     
+    //reconstruct_neighbors_from_vertices();
+    
+    //TEST_FAILED_IF(sphere->elements[1][0]->find_neighbor(sphere->elements[1][1]), "test_KSimplex_reconstruct_neighbors_from_vertices");
+
+    //test_ok("test_KSimplex_reconstruct_neighbors_from_vertices");
+}
+
+
+// ###################
+// Printing functions:
+// ###################
+
+// Rudimentary printing of the simplex data to stdout
+// (probably will be deprecated soon --- use print_detailed() instead)
+// TODO: ja bih ovo prepravio u wrapper za print_detailed() ili sl.
+void test_KSimplex_print(){
+    string name = "test";
+    int D = 2;
+    
+    // Initilize simplicial complex of dimension D, and an empty k-simplex:
+    SimpComp *simpComp = new(nothrow) SimpComp(name, D);
+
+    KSimplex *simpsmall = simpComp->create_ksimplex(0);
+    
+    KSimplex *newKSimplex = build_simplex_one_level_up(simpComp, simpsmall);    
+    
+    string expectedStr = R""""(Printing KSimplex:  k = 1, D = 2
+Printing colors:
+  Printing neighbors:
+Simplices at level k = 0:   Simplex, Simplex
+Simplices at level k = 1:   
+Simplices at level k = 2:   
+)"""";
+
+    // Redirect cout:
+    streambuf* oldCoutStreamBuf = cout.rdbuf();
+    std::ostringstream testStr;
+    cout.rdbuf( testStr.rdbuf() );
+
+    // All cout output is redirected to testStr:
+    newKSimplex->print();
+
+    // Restoring original cout:
+    cout.rdbuf( oldCoutStreamBuf );
+ 
+    // Printing all redirected cout output:
+    //cout << testStr.str();
+    //cout << expectedStr;
+
+    TEST_FAILED_IF(expectedStr != testStr.str(), "test_KSimplex_print");
+
+    test_ok("test_KSimplex_print");
+}
+
+// Prints the set of UniqueID colors of vertices of a given simplex,
+// in a human-readable format, "50(2-13-44)" or "(2-13-44)" and similar, to stdout
+// Works best if all vertices in the simplex are colored with UniqueIDColor
+void test_KSimplex_print_compact(){
+    string name = "test";
+    int D = 2;
+    
+    // Initilize simplicial complex of dimension D, and an empty k-simplex:
+    SimpComp *simpComp = new(nothrow) SimpComp(name, D);
+
+    KSimplex *simpsmall = simpComp->create_ksimplex(0);
+    
+    KSimplex *newKSimplex = build_simplex_one_level_up(simpComp, simpsmall);    
+    
+    string expectedStr = R""""(simplex)"""";
+
+    // Redirect cout:
+    streambuf* oldCoutStreamBuf = cout.rdbuf();
+    std::ostringstream testStr;
+    cout.rdbuf( testStr.rdbuf() );
+
+    // All cout output is redirected to testStr:
+    newKSimplex->print_compact();
+
+    // Restoring original cout:
+    cout.rdbuf( oldCoutStreamBuf );
+ 
+    // Printing all redirected cout output:
+    //cout << testStr.str();
+    //cout << expectedStr;
+
+    TEST_FAILED_IF(expectedStr != testStr.str(), "test_KSimplex_print_compact");
+
+    test_ok("test_KSimplex_print_compact");
+}
+
+// Detailed printing of the data of the simplex, in a nice human-readable
+// tabular format, to stdout
+// Prints the list of colors with their values, and a table of neighbors of a simplex
+// Relies on print_compact(), and works best if the entire complex is colored
+// with UniqueIDColor (see UniqueIDColor::colorize_entire_complex() for details)
+void test_KSimplex_print_detailed(){
+    string name = "test";
+    int D = 2;
+    
+    // Initilize simplicial complex of dimension D, and an empty k-simplex:
+    SimpComp *simpComp = new(nothrow) SimpComp(name, D);
+
+    KSimplex *simpsmall = simpComp->create_ksimplex(0);
+    
+    KSimplex *newKSimplex = build_simplex_one_level_up(simpComp, simpsmall);    
+    
+    string expectedStr = R""""(Simplex ID: 
+
+List of colors: 
+
+Table of neighbors: 
+---------------------------------------------
+Simplices at level k = 0:   Simplex, Simplex
+Simplices at level k = 1:   
+Simplices at level k = 2:   
+---------------------------------------------
+
+)"""";
+
+    // Redirect cout:
+    streambuf* oldCoutStreamBuf = cout.rdbuf();
+    std::ostringstream testStr;
+    cout.rdbuf( testStr.rdbuf() );
+
+    // All cout output is redirected to testStr:
+    newKSimplex->print_detailed();
+
+    // Restoring original cout:
+    cout.rdbuf( oldCoutStreamBuf );
+ 
+    // Printing all redirected cout output:
+    //cout << testStr.str();
+    //cout << expectedStr;
+
+    TEST_FAILED_IF(expectedStr != testStr.str(), "test_KSimplex_print_detailed");
+
+    test_ok("test_KSimplex_print_detailed");
+}
+
+// Constructs a string for printing the HTML link for a simplex,
+// with the syntax appropriate for the GUI
+// (it is heavily used by the GUI, not useful otherwise)
+void test_KSimplex_print_html(){
+    string name = "test";
+    int D = 2;
+    
+    // Initilize simplicial complex of dimension D, and an empty k-simplex:
+    SimpComp *simpComp = new(nothrow) SimpComp(name, D);
+
+    KSimplex *simpsmall = simpComp->create_ksimplex(0);
+    
+    KSimplex *newKSimplex = build_simplex_one_level_up(simpComp, simpsmall);    
+    
+    //newKSimplex->print_html();
+
+    string expectedStr = R""""()"""";
+
+    // Redirect cout:
+    streambuf* oldCoutStreamBuf = cout.rdbuf();
+    std::ostringstream testStr;
+    cout.rdbuf( testStr.rdbuf() );
+
+    // All cout output is redirected to testStr:
+    newKSimplex->print_html();
+
+    // Restoring original cout:
+    cout.rdbuf( oldCoutStreamBuf );
+ 
+    // Printing all redirected cout output:
+    //cout << testStr.str();
+    //cout << expectedStr;
+
+    TEST_FAILED_IF(expectedStr != testStr.str(), "test_KSimplex_print_html");
+
+    test_ok("test_KSimplex_print_html");
+}
+
+// Constructs a string for printing the non-HTML name for a simplex,
+// with the syntax appropriate for the GUI
+// (it is heavily used by the GUI, not useful otherwise)
+void test_KSimplex_print_non_html(){
+    string name = "test";
+    int D = 2;
+    
+    // Initilize simplicial complex of dimension D, and an empty k-simplex:
+    SimpComp *simpComp = new(nothrow) SimpComp(name, D);
+
+    KSimplex *simpsmall = simpComp->create_ksimplex(0);
+    
+    KSimplex *newKSimplex = build_simplex_one_level_up(simpComp, simpsmall);    
+    
+    //newKSimplex->print_non_html();
+    
+    string expectedStr = R""""()"""";
+
+    // Redirect cout:
+    streambuf* oldCoutStreamBuf = cout.rdbuf();
+    std::ostringstream testStr;
+    cout.rdbuf( testStr.rdbuf() );
+
+    // All cout output is redirected to testStr:
+    newKSimplex->print_non_html();
+
+    // Restoring original cout:
+    cout.rdbuf( oldCoutStreamBuf );
+ 
+    // Printing all redirected cout output:
+    //cout << testStr.str();
+    //cout << expectedStr;
+
+    TEST_FAILED_IF(expectedStr != testStr.str(), "test_KSimplex_print_non_html");
+
+    test_ok("test_KSimplex_print_non_html");
+}
+
+
 void run_tests(){
     test_kSimplex_default_constructor();
     test_kSimplex_default_destructor();
@@ -669,15 +1036,28 @@ void run_tests(){
     test_kSimplex_find_neighbor();
     test_kSimplex_get_uniqueID();
 
-test_build_simplex_one_level_up();
-test_attach_new_simplex_to_boundary();
-test_seed_single_simplex();
-test_seed_sphere();
-test_seed_sphere_intuitively();
-test_unseed_complex();
-test_unseed_everything();
+    test_build_simplex_one_level_up();
+    test_attach_new_simplex_to_boundary();
+    test_seed_single_simplex();
+    test_seed_sphere();
+    test_seed_sphere_intuitively();
+    test_unseed_complex();
+    test_unseed_everything();
 
     test_printing();
+    
+    test_KSimplex_collect_vertices();
+    test_KSimplex_find_neighbor();
+    test_KSimplex_add_neighbor();
+    test_KSimplex_delete_neighbor();
+    test_KSimplex_delete_all_neighbors();
+    test_KSimplex_reconstruct_neighbors_from_vertices();
+    
+    test_KSimplex_print();
+    test_KSimplex_print_compact();
+    test_KSimplex_print_detailed();
+    test_KSimplex_print_html();
+    test_KSimplex_print_non_html();
 }
 
 void test(){
